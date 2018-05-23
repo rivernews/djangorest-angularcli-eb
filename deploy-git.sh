e2ff52c7bba418779f4d61219364640178770ce3
Shaung Cheng <shaungcheng@Shaungs-MacBook-Pro.local>
2018-05-22 22:35:37 -0400

    fix

--- deploy-git.sh
+++ deploy-git.sh
@@ -7,16 +7,26 @@ else
     git_commit_msg="$1"
 fi
 
+# Delete comment to use these lines to deploy to different platforms
+#
+#
+#
+# Heroku
+#
 # if [[ $2 == 'dev' ]]
 # then
 #     heroku git:remote -a dev-iriversland
 # else
 #     heroku git:remote -a iriversland
 # fi
+# git push heroku # heroku git:remote -a iriversland || dev-iriversland
+#
+#
+# Github
+#
+# git push github
+# cd backend && eb deploy
 
 git add .
 git commit -m "$git_commit_msg"
 git push
-
-# git push heroku # heroku git:remote -a iriversland || dev-iriversland
-# git push github
